6ef67ffceed8f1ab4874e00f3eadcdb88b212311
Daniel Reule <dreule@1-E-27.42heilbronn.de>
2024-08-19 19:42:43 +0200

    fix2

--- dreule_42/ex00/functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUCNTIONS_H
-#define FUCNTIONS_H
-
-void	ft_putstr(char *str);
-int ft_strlen(char *str);
-
-#endif
--- dreule_42/ex00/main.c
+++
@@ -1,6 +0,0 @@
-#include "functions.h"
-int	main (int argc, char **argv)
-{
-	if(argc != 2 && argc != 3)
-		return(ft_putstr("Error"));
-}
